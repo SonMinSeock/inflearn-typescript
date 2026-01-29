a79670afff560f847096b8d8c5fbf4db4d62c0df
손민석 <zonins3@gmail.com>
2026-01-29 15:58:35 +0900

    섹션4. 사용자 정의 타입 가드

---
+++ section4/src/chapter4.ts
@@ -0,0 +1,44 @@
+/**
+ * 사용자 정의 타입 가드
+ */
+
+type Dog = {
+  name: string;
+  isBark: boolean;
+};
+
+type Cat = {
+  name: string;
+  isScratch: boolean;
+};
+
+type Animal = Dog | Cat;
+
+// is 키워드
+// 만약 반환 값이 true이면 animal은 Dog 타입이다.
+function isDog(animal: Animal): animal is Dog {
+  return (animal as Dog).isBark !== undefined;
+}
+
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isScratch !== undefined;
+}
+
+function warning(animal: Animal) {
+  if (isDog(animal)) {
+    // 강아지
+    console.log('animal dog');
+    animal;
+  } else if (isCat(animal)) {
+    // 고양이
+    console.log('animal cat');
+    animal;
+  }
+}
+
+const dog: Dog = {
+  name: '뚜비',
+  isBark: true,
+};
+
+warning(dog);
