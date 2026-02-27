f2ef72d91b9176dbc261af6ce5d620f29619e0ba
손민석 <zonins3@gmail.com>
2026-02-27 15:54:56 +0900

    섹션 3. 기본타입간의 호환성 코드 추가

--- section2/src/chapter3.ts
+++ section2/src/chapter3.ts
@@ -1,22 +1,22 @@
 const user: {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 } = {
-    id: 1,
-    name: "손민석",
+  id: 1,
+  name: '손민석',
 };
 
 const user2: {
-    id?: number;
-    name: string;
+  id?: number;
+  name: string;
 } = {
-    name: "아이유"
-}
+  name: '아이유',
+};
 
 const config: {
-    readonly apiKey: string;
+  readonly apiKey: string;
 } = {
-    apiKey: "My API Key",
+  apiKey: 'My API Key',
 };
 
 // config.apiKey = "23"
